e30032424e806e11211b8099994c36dd9f483beb
SenorPenguin <jimmy.dwyer25@gmail.com>
2018-11-07 19:47:13 -0600

    Gif API data appears, need to push to page.

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 var topics = ["skateboarding","soccer","tennis","football","baseball","swimming"];
 
+//create a for loop that converts any user input into data-name to pull gifs
+function displayArray(){
 for(var i=0;i<topics.length;i++){
-    $("#topic-buttons").append('<button id="add-gif" data-name="' + topics[i] + '">' + topics[i]);
+    $("#topic-buttons").append('<button id="gif-button" data-name="' + topics[i] + '">' + topics[i]);
 };
-
+};
 var topicsArray = $("#to-do").val();
-
-
-// topics = $("<button>");
-// $("#to-do").append(topics);
-// function array(){
-// toDoArray.push(toDoTask);
-
+displayArray();
 
 
 
 $("#add-gif").on("click", function(event) {
     event.preventDefault();
-    var gifName = $(this).val("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      gifName + "&api_key=dc6zaTOxFJmzC&limit=1";
+    $("#topic-buttons").empty();
+    topics.push($("#gif-input").val());
+      displayArray();
+});
+
+$("#gif-button").on("click", function(){
+var gifName = $(this).val("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      gifName + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
         url: queryURL,
         method: "GET"
       })
-    
-});
+      .then(function(response){
+        console.log(queryURL);
+        console.log(response);
+        //Gif API data appears, need to push to page
+          var results = response.data;
+          for(var i=0;i<results.length;i++);
 
+      })
 
-
+    });
